e712c7c6948a02fd90761d81abf8c2a88e6ef203
milen404ka <mmieno4ka4@gmail.com>
2022-07-13 13:33:21 +0300

    небольшие изменения

--- src/components/Products/ProductItem.tsx
+++ src/components/Products/ProductItem.tsx
@@ -1,24 +1,20 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import "../../styles/buttonDelete.scss";
-import Context, {TodoContextType} from "../../context/context";
 
 //MUI
 import IconButton from '@mui/material/IconButton';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
-
 const ProductItem = ({todo}: any) => {
-
-    const {newCRUD} = useContext(Context) as TodoContextType;
 
     return (
             <div className="product-card">
                     {/*DESCRIPTION*/}
-                    <div className="product-info" style={{textDecoration: todo.complete ? " " : ""}}
-                     onClick={() => newCRUD.updateTask(todo.id)}>
+                    <div className="product-info" onClick={() => ''}>
                         <p className="todoName">{todo.name}</p>
+                        <p className="todoSubstance">Действующее вещество:</p>
                         <p className="todoSubstanceName">{todo.substance.name}</p>
-                        <div className="todoImg"><img src={todo.image} width='120px' height='120px'/></div>
+                        <img src={todo.image} width='120px' height='120px'/>
                     </div>
 
                     {/*BUTTON*/}
