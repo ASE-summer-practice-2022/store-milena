3c79ddcdf10640f067c7878344ceae58169cf39b
milen404ka <mmieno4ka4@gmail.com>
2022-07-17 17:00:20 +0300

    enum

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
-import data from "./data/data.json";
 import { Provider } from 'mobx-react';
-import './styles/App.scss';
+import { storeNames } from "./stores/Enum";
+//data
+import data from "./data/data.json";
 import JsonData from './data/data.json';
+//styles and themes
+import './styles/App.scss';
 import './styles/App.scss';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import {darkTheme} from './styles/theme/DarkTheme';
-
+import { darkTheme } from './styles/theme/DarkTheme';
 //components
 import Header from "./components/system/Header";
 import CardList from "./components/CardList";
 import CardStore from "./stores/CardStore";
 
-
 function App() {
-  const { products } = JsonData.data;
+    const {products} = JsonData.data;
+    const cardStore = new CardStore();
+    cardStore.setProducts(products);
 
-  const cardStore = new CardStore();
-  cardStore.setProducts(products);
-
-  const stores = {
-    'CardStore': cardStore
-  };
+    const stores = {
+        [storeNames.CardStoreName]: cardStore,
+    };
 
   return (
       <Provider {...stores}>
